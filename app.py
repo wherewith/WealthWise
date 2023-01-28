8e49bbd69d60303ebfc88b6dc31733318441e76a
rahul-rajendran3 <rahul.rajendran2724@gmail.com>
2023-01-28 13:35:52 -0600

    fico

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
 	return render_template("index.html")
 
+@app.route("/fico")
+def fico():
+	return render_template("fico.html")
+
 if __name__ == '__main__':
 	app.run(debug=True)
