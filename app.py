8dfd59effec3d3c9a9a4afd0d15b6a1d42650ae6
Kunal Jain <55034053+wherewith@users.noreply.github.com>
2023-01-28 13:55:53 -0600

    for looped navbar

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def index():
 	return render_template("index.html")
 
+@app.route("/about/")
+def about():
+	return render_template("about.html")
+
 @app.route("/fico")
 def fico():
 	return render_template("fico.html")
