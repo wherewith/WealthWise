9c19b4f050736cca8a899e1381662ec2648ec6af
Kunal Jain <55034053+wherewith@users.noreply.github.com>
2023-01-28 11:45:47 -0600

    add flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,13 @@
 module.exports = {
   content: [
     "./templates/**/*.html",
-    "./static/src/**/*.js"
+    "./static/src/**/*.js",
+    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+      require("flowbite/plugin")
+  ],
 }
