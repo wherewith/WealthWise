6d02f0a765740441b8ad768bfd22f15a51564d59
rahul-rajendran3 <rahul.rajendran2724@gmail.com>
2023-01-28 20:11:05 -0600

    student page

--- app.py
+++ app.py
@@ -10,14 +10,17 @@ def index():
 def about():
 	return render_template("about.html")
 
-@app.route("/wiki/")
 @app.route("/wiki/fico/")
 def wiki_fico():
 	return render_template("fico.html")
 
+@app.route("/wiki/students")
+def wiki_students():
+	return render_template("students.html")
 @app.errorhandler(404)
 def page_not_found(e):
 	return render_template("404.html")
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
