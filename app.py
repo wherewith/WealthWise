4876e2a7b0ae66db22a9334c1b7fb4ce0cbd0b36
Kunal Jain <55034053+wherewith@users.noreply.github.com>
2023-01-28 21:48:17 -0600

    route fix

--- app.py
+++ app.py
@@ -14,9 +14,10 @@ def about():
 def wiki_fico():
 	return render_template("fico.html")
 
-@app.route("/wiki/students")
+@app.route("/wiki/students/")
 def wiki_students():
 	return render_template("students.html")
+
 @app.errorhandler(404)
 def page_not_found(e):
 	return render_template("404.html")
