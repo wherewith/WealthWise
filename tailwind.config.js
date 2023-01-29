ddea962df8cd3dbf364bc99825e0c0947a5ccfa2
Kunal Jain <55034053+wherewith@users.noreply.github.com>
2023-01-29 04:01:10 -0600

    homepage + custom css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./templates/**/*.html",
-    "./static/src/**/*.js",
-    "./node_modules/flowbite/**/*.js"
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-      require("flowbite/plugin")
-  ],
+    content: [
+        "./templates/**/*.html",
+        "./static/src/**/*.js",
+        "./node_modules/flowbite/**/*.js"
+    ],
+    theme: {
+    },
+    plugins: [
+        require("flowbite/plugin")
+    ],
 }
